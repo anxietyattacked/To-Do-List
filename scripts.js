18cef4a2b5a4faf7f64258340aa13582444f4784
Michaell Bergerson <michaelbergerson@gmail.com>
2020-09-15 14:21:27 -0700

    improved functionality and design

--- scripts.js
+++ scripts.js
@@ -4,18 +4,30 @@ list = document.getElementById("list")
 const savebtn = document.querySelector(".save")
 let saveArray = []
 
-// const save = localStorage.getItem(save)
-// const todoArray = save.toArray()
-// todoArray.forEach(todo => {
-// createTodo()
-// });
+const save = localStorage.getItem("save")
+if(save != undefined) {
+    let todoArray = save.split(",")
+    console.log(todoArray)
+    if (todoArray === [[""]]) {
+       console.log(todoArray)
+} else {
+    todoArray.forEach(todo => {
+        createTodo(todo)
+        console.log(todoArray)
+        });
+}
+}
+
+   
 
 // Event Listeners
-addTodo.addEventListener("click", createTodo)
+addTodo.addEventListener("click", () => {
+    createTodo(todoInput.value)
+})
 
 
 // Functions
-function createTodo() {
+function createTodo(elm) {
 const div = document.createElement("div")
 div.className = "todo-div"
 const btnDiv = document.createElement("div")
@@ -27,18 +39,29 @@ save.className = "save"
 const deleteBtn = document.createElement("button")
 deleteBtn.textContent = "Delete"
 deleteBtn.className = "delete"
-todo.textContent = todoInput.value
+todo.textContent = elm
 list.appendChild(div)
 div.appendChild(todo)
 div.appendChild(btnDiv)
 btnDiv.appendChild(save)
 btnDiv.appendChild(deleteBtn)
-
+console.log("clicked")
 deleteBtn.addEventListener("click", deleteTodo)
 save.addEventListener("click", saved)
 }
 function deleteTodo(e) {
+let todoArray = save.split(",")
 const del = e.target
+const li = del.parentNode.parentNode.childNodes[0].innerText.toString()
+const filtered = todoArray.filter(item => {
+    return item !== li
+})
+saveArray.filter(item => {
+    return item !== li
+})
+localStorage.removeItem("save")
+localStorage.setItem("save", filtered.toString())
+console.log(localStorage.getItem("save"))
 del.parentNode.parentNode.remove()
 }
 function saved(e){
